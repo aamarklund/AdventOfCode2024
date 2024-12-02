76b9e9f5329b9418b1308554310c65185fe9aef1
Anton Marklund <anton.marklund@sogeti.com>
2024-12-02 18:15:51 -0100

    day 2

--- AdventOfCode2024.Core/Program.cs
+++ AdventOfCode2024.Core/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2024.Core.Solutions;
 
-var day = new Day1("day1");
+var day = new Day2("day2");
 day.Solution();
---
+++ AdventOfCode2024.Core/Solutions/Day2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Core.Solutions
+{
+    internal class Day2(string inputFileName) : BaseDay(inputFileName)
+    {
+        internal override int Part1(List<string> input)
+        {
+            var safeReports = 0;
+            foreach (var line in input) 
+            { 
+                var report = line.Split(" ").Select(int.Parse);
+                if (IsSafe(report))
+                    safeReports++;
+
+            }
+            return safeReports;
+        }
+
+        private static bool IsSafe(IEnumerable<int> report)
+        {
+            var difPairsLessOne =report 
+                .Zip(report.Skip(1)).ToList()
+                .Select(a => (a.Second - a.First, a.First - a.Second));
+            return 
+                difPairsLessOne.All(x => x.Item1 <= 3 && 1 <= x.Item1) ||
+                difPairsLessOne.All(x => x.Item2 <= 3 && 1 <= x.Item2);
+        }
+
+
+
+        internal override int Part2(List<string> input)
+        {
+            var safeReports = 0;
+            foreach (var line in input)
+            {
+                var report = line.Split(" ").Select(int.Parse);
+
+                if (IsSafe(report))
+                    safeReports++;
+                else
+                {
+                    var reportList = report.ToList();
+                    for (int i = 0; i < reportList.Count; i++)
+                    {
+                        var reportLessOne = reportList.ToList();
+                        reportLessOne.RemoveAt(i);
+                        if (IsSafe(reportLessOne))
+                        {
+                            safeReports++;
+                            break;
+                        }
+                    }
+                }
+
+            }
+            return safeReports;
+        }
+    }
+}
