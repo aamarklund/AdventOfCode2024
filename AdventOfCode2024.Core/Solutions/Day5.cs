77992282bde02f4f25c7a752b7c6a323a66c49fb
Anton Marklund <anton.marklund@sogeti.com>
2024-12-09 08:16:00 +0100

    day 5

--- AdventOfCode2024.Core/Solutions/Day5.cs
+++ AdventOfCode2024.Core/Solutions/Day5.cs
@@ -10,12 +10,56 @@ namespace AdventOfCode2024.Core.Solutions
     {
         internal override int Part1(List<string> input)
         {
-            return 0;
+            var result = 0;
+            var (rules, updates) = ParseInput(input);
+
+            var visited = new List<int>();
+            foreach(var update in updates)
+            {
+                var upd = update.Split(",").Select(int.Parse).ToList();
+                foreach(var seq in upd)
+                {
+                    if (rules.Any(x => x.Item1 == seq)) {
+                        visited.Add(seq);
+                    }
+                    else if(visited.Contains(rules.Where(x => x.Item1 == seq).First().Item1))
+                    {
+                        visited.Add(seq);
+                    }
+                }
+                if(visited.Count == upd.Count)
+                {
+                    result += GetMiddlePage(update.Split(","));
+                } 
+
+            }
+            return result;
         }
 
         internal override int Part2(List<string> input)
         {
             return 0;
+        }
+
+        private static int GetMiddlePage(string[] nums)
+        {
+            return int.Parse(nums[nums.Length / 2]);
+        }
+
+        private static (List<(int, int)> rules, List<string> updates) ParseInput(List<string> input)
+        {
+            var longString = string.Join("\n", input);
+            var orderingRules = longString.Split("\n\n")[0].Split("\n");
+            var rules = new List<(int, int)>();
+
+            foreach (var orderingRule in orderingRules)
+            {
+                var split = orderingRule.Split('|').Select(int.Parse).ToArray();
+                rules.Add((split[0], split[1]));
+            }
+            var updates = longString.Split("\n\n")[1].Split("\n").ToList();
+
+            return (rules, updates);
         }
     }
 }
