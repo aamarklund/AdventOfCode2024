bddd6494c96be4955b92dedffbd5e763953d59bb
Anton Marklund <anton.marklund@sogeti.com>
2024-12-22 10:13:37 +0100

    day 22 del 2

--- AdventOfCode2024.Core/Solutions/Day22.cs
+++ AdventOfCode2024.Core/Solutions/Day22.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,22 +11,59 @@ namespace AdventOfCode2024.Core.Solutions
     {
         internal override int Part1(List<string> input)
         {
+            var lines = input.Select(long.Parse);
             long total = 0;
-            foreach (var line in input)
+            foreach (var line in lines)
             {
-                long num = long.Parse(line);
+                long num = line;
                 for (int i = 0; i < 2000; i++)
                 {
                     num = GetNextStep(num);
                 }
                 total += num;
             }
-            return (int)total;
+            return (int)total; // DEBUG
         }
 
         internal override int Part2(List<string> input)
         {
-            return -1;
+            var lines = input.Select(long.Parse);
+            var seqTotal = new Dictionary<(long, long, long, long), long>();
+            foreach (var line in lines)
+            {
+                long num = line;
+                var buyer = new List<long>() { num % 10 };
+                for (int i = 0; i < 2000; i++)
+                {
+                    num = GetNextStep(num);
+                    buyer.Add(num % 10);
+                }
+
+                var seenSeqs = new HashSet<(long, long, long, long)>();
+                for (int i = 0; i < buyer.Count - 4; i++)
+                {
+                    long a = buyer[i];
+                    long b = buyer[i + 1];
+                    long c = buyer[i + 2];
+                    long d = buyer[i + 3];
+                    long e = buyer[i + 4];
+
+                    var seq = (b - a, c - b, d - c, e - d);
+                    if(seenSeqs.Contains(seq))
+                    {
+                        continue;
+                    }
+                    seenSeqs.Add(seq);
+                    if(!seqTotal.ContainsKey(seq))
+                    {
+                        seqTotal[seq] = 0;
+                    }
+                    seqTotal[seq] += e;
+
+                }
+            }
+
+            return (int)seqTotal.Max(x => x.Value);
         }
 
         private static long GetNextStep(long number)
